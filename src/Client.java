f46167fdcdd078a1f2a5994689328a0fc9dba116
TahaBakk <tahabakk2@gmail.com>
2017-04-24 16:57:23 +0200

    creado metodo para calcular sin menu

--- src/Client.java
+++ src/Client.java
@@ -8,34 +8,28 @@ import java.util.Scanner;
  */
 public class Client {
 
+    private static int operacionTipo = 0;
+    private static double num1 = 0;
+    private static double num2 = 0;
+
     public static void main(String[] args) {
         ServerInterficie coneccion = null;
         Scanner sc = new Scanner(System.in);
         int operacion = 0;
-        double num1 =0;
-        double num2 =0;
+
         //esto es para conectarse al servidor
         try{
             System.out.println("Localitzant registre d'objectes remots ...");
             Registry registry = LocateRegistry.getRegistry("localhost",5555);
             System.out.println("Obtenint l'objecte remot...");
 
-            //dependiendo de la opcion seleccionada en el menu ara una de las siguientes funciones
             System.out.println("-------------------------------------------------------");
-            System.out.println("Menu (seleccione una opción)");
-            System.out.println("1.Sumar");
-            System.out.println("2.Restar");
-            System.out.println("3.dividir");
-            System.out.println("4.Multiplicar");
+            System.out.println("Introduce la operación que desea realizar: (ej:5 + 5)");
             System.out.println("-------------------------------------------------------");
+            String operacionRealizar = sc.nextLine();
 
-            operacion = sc.nextInt();
-
-            //Pedimos los numeros a calcular
-            System.out.println("Dame el primer numero a calcular");
-            num1 = sc.nextDouble();
-            System.out.println("Dame el segundo numero a calcular");
-            num2 = sc.nextDouble();
+            operacionClient(operacionRealizar);
+            operacion = operacionTipo;
 
             //establecemos la connexión
                 coneccion = (ServerInterficie) registry.lookup("conexion");
@@ -67,4 +61,45 @@ public class Client {
         System.out.println("FI");
 
     }
+
+    private static void operacionClient(String operacion) {
+
+        //usamos el split para que nos separe los valores que estan antes de la operacion y el de despues
+        // y para saber que tipo de operacion es
+        if (operacion.contains("+")){
+            operacionTipo = 1;
+            String [] datos = operacion.split("\\+");
+            num1 = Double.parseDouble(datos[0]);
+            num2 = Double.parseDouble(datos[1]);
+        }
+
+        if (operacion.contains("-")){
+            operacionTipo = 2;
+            String [] datos = operacion.split("-");
+            num1 = Double.parseDouble(datos[0]);
+            num2 = Double.parseDouble(datos[1]);
+        }
+
+        if (operacion.contains("/")){
+            operacionTipo = 3;
+            String [] datos = operacion.split("/");
+            num1 = Double.parseDouble(datos[0]);
+            num2 = Double.parseDouble(datos[1]);
+        }
+
+        if (operacion.contains("*")){
+            operacionTipo = 4;
+            String [] datos = operacion.split("\\*");
+            num1 = Double.parseDouble(datos[0]);
+            num2 = Double.parseDouble(datos[1]);
+        }
+
+        if (!operacion.contains("*") && !operacion.contains("/") && !operacion.contains("-") && !operacion.contains("+")){
+            throw new RuntimeException("Se ha producido, signo no localizado ");
+        }
+
+    }
+
+
+
 }
